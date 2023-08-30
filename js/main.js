1e8c1aebbeb9153a241528fd68084ccc14fafa96
plastikSun <marcom2k01@gmail.com>
2023-08-30 16:36:31 +0200

    add: next and prev functions. infinite loop logic

--- js/main.js
+++ js/main.js
@@ -4,8 +4,33 @@ const sliderApp = createApp({
     data() {
         return {
             slidesData: slides,
+            current: 0,
+            maxSlides: slides.length - 1
         };
     },
+
+    methods: {
+        next() {
+            if (this.current < this.maxSlides ) {
+                this.current++;
+            } else {
+                this.current = 0;
+            }
+            
+            console.log(this.current)
+            
+        },
+
+        prev() {
+            if (this.current <= 0 ) {
+                this.current = this.maxSlides;
+            } else {
+                this.current--;
+            }
+            
+            console.log(this.current)
+        },
+    }
 
 }).mount("#slider")
 
